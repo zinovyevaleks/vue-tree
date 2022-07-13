3c0a309d6a5fe8f2eebe3f74548a9a7cc70d2b50
Александр <zinovyevaleks@yandex.ru>
2022-07-14 00:05:09 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TreeView/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TreeView from "./components/TreeView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TreeView
+},
+};
 </script>
 
 <style lang="scss">
